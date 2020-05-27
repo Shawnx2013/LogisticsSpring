d15360b19ccf83b4efe87b59e383fb01b2b363cf
Shaohui Xu <shao2013x@gmail.com>
2020-05-27 14:38:52 -0400

    set up template

--- src/main/java/com/log/LogisticsSpring/LogisticsSpringApplication.java
+++ src/main/java/com/log/LogisticsSpring/LogisticsSpringApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogisticsSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticsSpringApplication.class, args);
 	}
-
 }

129961b08aa62865adea15cd44578c6d3fd92663
Shaohui Xu <shao2013x@gmail.com>
2020-05-27 13:29:22 -0400

    Initial commit

---
+++ src/main/java/com/log/LogisticsSpring/LogisticsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.log.LogisticsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsSpringApplication.class, args);
+	}
+
+}

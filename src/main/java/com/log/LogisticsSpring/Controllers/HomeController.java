a877a95b5a1512e2fbecc4b100ec4e00e013802d
yikang <yikangchen112@gmail.com>
2020-05-27 19:12:33 -0400

    added login stuff

--- src/main/java/com/log/LogisticsSpring/Controllers/HomeController.java
+++ src/main/java/com/log/LogisticsSpring/Controllers/HomeController.java
@@ -1,9 +1,8 @@
 package com.log.LogisticsSpring.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -14,3 +13,4 @@ public class HomeController {
         return "index";
     }
 }
+

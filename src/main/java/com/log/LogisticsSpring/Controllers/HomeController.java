6c9829c404aad14738e6b1f6d2d19f7af0c13458
Shaohui Xu <shao2013x@gmail.com>
2020-05-27 20:03:30 -0400

    set up log in

--- src/main/java/com/log/LogisticsSpring/Controllers/HomeController.java
+++ src/main/java/com/log/LogisticsSpring/Controllers/HomeController.java
@@ -1,16 +1,28 @@
 package com.log.LogisticsSpring.Controllers;
 
-import org.springframework.web.bind.annotation.*;
+import com.log.LogisticsSpring.Models.User;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String greeting(@RequestParam(name="name", required=false,
-            defaultValue="World") String name, Model model){
-        model.addAttribute("name", name);
+    public String getIndex(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("name", "Customer");
         return "index";
     }
-}
 
+    @PostMapping("/")
+    public String login(Model model, User user){
+        String name = user.getUsername();
+        model.addAttribute("name", name);
+        model.addAttribute("user", user);
+        return "result";
+    }
+
+}
--- src/main/java/com/log/LogisticsSpring/Models/User.java
+++ src/main/java/com/log/LogisticsSpring/Models/User.java
@@ -1,13 +1,13 @@
-package com.log.LogisticsSpring.Controllers;
+package com.log.LogisticsSpring.Models;
 
 public class User {
     private String username;
     private String password;
-
+    private String email;
     public String getUsername() {
         return username;
     }
-
+    public String getEmail() {return email;}
     public String getPassword() {
         return password;
     }
@@ -16,8 +16,13 @@ public class User {
         this.username = username;
     }
 
+    public void setEmail(String email) { this.email = email; }
     public void setPassword(String password) {
         this.password = password;
     }
 
+    @Override
+    public String toString(){
+        return "username: " + username + "\npassword: " + password;
+    }
 }
